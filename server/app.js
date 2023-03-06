9d8c311957815218692b75f749f624d30d097e10
Maximiliano <brotzumaxx@gmail.com>
2023-03-06 14:05:42 -0300

    Express session implementado

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import routerSession from './routes/routerSession.js'
+import session from 'express-session';
+//Passport
+import passport from 'passport';
+import configurePassport from './passport-config.js';
+//Routes
+import routerSession from './routes/routerSession.js';
+import routerHome from './routes/routerHome.js';
 
 const app = express();
 
@@ -8,11 +14,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//Session
+app.use(session({
+    secret: 'secreto',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+configurePassport(passport);
+
 // Configuración del motor de vistas
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 //Rutas
 app.use('/session', routerSession);
+app.use('/home', routerHome);
 
 export default app;
